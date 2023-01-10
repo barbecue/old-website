eae6bcea4b3b06d1fe2af54756767dc4deb7757d
barbecue <barbeku@protonmail.com>
2023-01-10 20:42:14 +0300

    Update mal.ts

--- src/server/api/mal.ts
+++ src/server/api/mal.ts
@@ -1,7 +1,8 @@
 import malScraper from 'mal-scraper';
+const runtimeConfig = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
-    const List = await malScraper.getWatchListFromUser('baybeku', 0, 'anime');
+    const List = await malScraper.getWatchListFromUser(runtimeConfig.mal_username, 0, 'anime');
     return {
         data: List.sort((anime1, anime2) => anime2.updatedAt-anime1.updatedAt).slice(0, 6)
     }
