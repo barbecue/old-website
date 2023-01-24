521e381c638cc244c92157afbc48cbfa74a1fb69
barbecue <barbeku@protonmail.com>
2023-01-24 19:15:46 +0300

    :recycle: Bug fix.

--- src/components/Cards/Discord.vue
+++ src/components/Cards/Discord.vue
@@ -5,8 +5,9 @@ const { data, pending, error, refresh } = await useFetch(() => `https://api.lany
 <template>
   <div class="bg-neutral-200 dark:bg-neutral-900 rounded-md p-5 px-32 flex flex-col items-center space-y-2">
     <div v-if="!pending && !error" class="flex justify-end items-end">
-      <nuxt-img draggable="false" :src="`https://cdn.discordapp.com/avatars/${data.data.discord_user.id}/${data.data.discord_user.avatar}.${data.data.discord_user.avatar.startsWith('a_') ? 'gif' : 'webp'}?size=128`" :alt="`${data.data.discord_user.username}#${data.data.discord_user.discriminator}`" class="relative w-24 h-24 rounded-full"></nuxt-img>
-      <Status :status="data.data.discord_status"/>
+      <nuxt-img v-if="data.data.discord_user.avatar" draggable="false" :src="`https://cdn.discordapp.com/avatars/${data.data.discord_user.id}/${data.data.discord_user.avatar}.${data.data.discord_user.avatar.startsWith('a_') ? 'gif' : 'webp'}?size=128`" :alt="`${data.data.discord_user.username}#${data.data.discord_user.discriminator}`" class="relative w-24 h-24 rounded-full"></nuxt-img>
+      <nuxt-img v-else draggable="false" src="https://cdn.discordapp.com/embed/avatars/5.png" :alt="`${data.data.discord_user.username}#${data.data.discord_user.discriminator}`" class="relative w-24 h-24 rounded-full"></nuxt-img>
+      <Status :status="data.data.discord_status" />
     </div>
     <div v-else class="flex justify-end items-end">
       <div class="relative bg-neutral-300 dark:bg-neutral-800 animate-pulse w-24 h-24 rounded-full"></div>
