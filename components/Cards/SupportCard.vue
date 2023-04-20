69b089bc8da1231fa4e8094bf1ba88e0bf9b973d
barbecue <barbeku@pm.me>
2023-04-21 01:45:54 +0300

    fix: Changed design of Support Card

--- components/Cards/SupportCard.vue
+++ components/Cards/SupportCard.vue
@@ -14,11 +14,12 @@ function visible() {
 
 <template>
   <div
+    v-motion-slide-bottom
     :class="[visible() ? 'flex' : 'hidden']"
-    class="fixed right-0 bottom-0 z-10 p-4"
+    class="fixed bottom-0 right-0 z-10 p-4"
   >
     <div
-      class="flex flex-col items-start gap-2 rounded-md border border-neutral-500/20 bg-neutral-800/90 p-4 shadow-xl md:bg-neutral-800/70"
+      class="flex flex-col items-start gap-2 rounded-md border border-neutral-500/20 bg-neutral-200/80 p-4 shadow-xl dark:bg-neutral-800/90 md:bg-neutral-200/90 dark:md:bg-neutral-800/70"
     >
       <div class="flex flex-row items-center justify-between gap-4">
         <span
@@ -27,7 +28,7 @@ function visible() {
           Support this website on github!</span
         >
         <button
-          class="items-center text-white focus:outline-none"
+          class="items-center text-black focus:outline-none dark:text-white"
           @click="disable"
         >
           <Icon name="ri:close-fill" class="h-5 w-5" />
@@ -36,7 +37,7 @@ function visible() {
       <nuxt-link
         to="https://github.com/barbecue/website"
         target="_blank"
-        class="flex items-center gap-1.5 rounded-md border border-zinc-700/50 bg-neutral-800/90 p-2 px-4 text-white backdrop-blur-3xl transition duration-300 hover:bg-neutral-700/50 focus:outline-none"
+        class="flex items-center gap-1.5 rounded-md border border-zinc-400/40 bg-neutral-300/40 p-2 px-4 backdrop-blur-3xl transition duration-300 hover:bg-neutral-300 focus:outline-none dark:border-zinc-700/50 dark:bg-neutral-800/90 dark:text-white dark:hover:bg-neutral-700/50"
       >
         <Icon name="ri:github-fill" />
         <span>View On Github</span>
