527c0c36057bc371eb394f0ee30aeea1384d300a
barbecue <barbeku@pm.me>
2023-05-26 20:36:44 +0300

    feat: Added tools page

---
+++ components/Cards/ToolCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface Data {
+  title: string;
+  description: string;
+  icon: string;
+  url: string;
+}
+const props = defineProps<{
+  data: Data;
+}>();
+</script>
+<template>
+  <nuxt-link
+    :to="props.data.url"
+    class="flex max-h-36 cursor-pointer items-center rounded-md border border-gray-300 px-4 py-6 text-white duration-300 hover:bg-gray-200 dark:border-neutral-800 dark:hover:bg-neutral-900 md:py-0"
+  >
+    <div class="flex items-center justify-between gap-5">
+      <Icon :name="props.data.icon" class="h-26 w-26"></Icon>
+      <div class="flex flex-col space-y-1.5">
+        <span class="text-lg text-black dark:text-white">
+          {{ props.data.title }}
+        </span>
+        <p class="hidden text-[13px] leading-5 text-neutral-500">
+          {{ props.data.description.slice(0, 120) }}...
+        </p>
+        <p class="text-[15px] leading-5 text-neutral-500">
+          {{ props.data.description.slice(0, 50) }}...
+        </p>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
---
+++ pages/tools/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+useHead({
+  title: "Tools",
+});
+
+const tools = [];
+</script>
+
+<template>
+  <div
+    v-motion-slide-bottom
+    class="relative mx-auto max-w-7xl items-center px-12 md:px-24"
+  >
+    <h1 class="text-2xl text-neutral-500">Tools I Made</h1>
+    <div class="mt-4 grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-4">
+      <ToolCard
+        v-for="tool in tools"
+        :key="tool.title"
+        v-if-="tools.length"
+        :data="tool"
+      />
+      <h2 v-else- class="dark:text-neutral-500">
+        Nothing to see here (for now)
+      </h2>
+    </div>
+  </div>
+</template>
