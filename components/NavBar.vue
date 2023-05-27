1a7d0d53d5b494409d82a6f9a69f40515509d564
barbecue <barbeku@pm.me>
2023-05-27 22:34:50 +0300

    feat: Made NavBar sticky, changed logo

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,35 +1,54 @@
 <script setup lang="ts">
 const menu = ref(false);
+const items = [
+  {
+    title: "Home",
+    icon: "solar:home-2-bold-duotone",
+    to: "/",
+  },
+  {
+    title: "Tools",
+    icon: "solar:sledgehammer-bold-duotone",
+    to: "/tools",
+  },
+  {
+    title: "Blog",
+    icon: "solar:document-bold",
+    to: "/blog",
+  },
+];
+const scrollPosition = ref();
+
+function updateScroll() {
+  scrollPosition.value = window.scrollY;
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", updateScroll);
+});
 </script>
 <template>
   <div
-    class="relative mx-auto flex max-w-7xl items-center justify-between px-12 py-7 md:px-24"
+    class="navbar sticky top-0 z-50 mx-auto flex max-w-7xl items-center justify-between rounded-md px-12 py-4 transition-all duration-300 md:px-24"
   >
     <div class="flex flex-row items-center justify-center space-x-5">
       <NuxtLink to="/" class="text-4xl text-black dark:text-white"
-        >tuna.one</NuxtLink
-      >
+        ><nuxt-img
+          src="/assets/images/logo.svg"
+          class="duration h-14 w-14 transform transition hover:scale-110"
+      /></NuxtLink>
     </div>
     <div class="hidden flex-row items-center space-x-3.5 md:flex">
       <BLink
-        to="/"
-        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-4 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-[#050505] dark:text-neutral-500 dark:hover:text-white"
-        ><Icon name="solar:home-2-bold-duotone"></Icon> <span>Home</span></BLink
-      >
-      <BLink
-        to="/tools"
-        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-4 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-[#050505] dark:text-neutral-500 dark:hover:text-white"
-        ><Icon name="solar:sledgehammer-bold-duotone"></Icon>
-        <span>Tools</span></BLink
-      >
-      <BLink
-        to="/blog"
-        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-4 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-[#050505] dark:text-neutral-500 dark:hover:text-white"
-        ><Icon name="solar:document-bold"></Icon> <span>Blog</span></BLink
+        v-for="item in items"
+        :key="item.title.toLowerCase()"
+        :to="item.to"
+        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-4 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-neutral-900 dark:text-neutral-500 dark:hover:text-white"
+        ><Icon :name="item.icon"></Icon> <span>{{ item.title }}</span></BLink
       >
       <button
         aria-label="theme-switch"
-        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-4 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-[#050505] dark:text-neutral-500 dark:hover:text-white"
+        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-4 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-neutral-900 dark:text-neutral-500 dark:hover:text-white"
         @click="
           $colorMode.preference === 'light'
             ? ($colorMode.preference = 'dark')
@@ -59,20 +78,11 @@ const menu = ref(false);
   <div class="mx-auto max-w-7xl items-center px-12 pb-4 md:px-24">
     <div v-if="menu" class="flex flex-col gap-2 md:hidden">
       <BLink
-        to="/"
+        v-for="item in items"
+        :key="item.title.toLowerCase()"
+        :to="item.to"
         class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-2.5 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-neutral-900 dark:text-neutral-500 dark:hover:text-white"
-        ><Icon name="solar:home-2-bold-duotone"></Icon> <span>Home</span></BLink
-      >
-      <BLink
-        to="/tools"
-        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-2.5 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-neutral-900 dark:text-neutral-500 dark:hover:text-white"
-        ><Icon name="solar:sledgehammer-bold-duotone"></Icon>
-        <span>Tools</span></BLink
-      >
-      <BLink
-        to="/blog"
-        class="flex flex-row items-center justify-center space-x-1 rounded-md bg-neutral-300 px-2.5 py-1 text-neutral-500 transition duration-300 hover:text-black focus:outline-none dark:bg-neutral-900 dark:text-neutral-500 dark:hover:text-white"
-        ><Icon name="solar:document-bold"></Icon> <span>Blog</span></BLink
+        ><Icon :name="item.icon"></Icon> <span>{{ item.title }}</span></BLink
       >
       <button
         aria-label="theme-switch"
