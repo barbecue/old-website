3249e28a28f367b4cddbb9efacec0144d36d23e6
BBQ <barbeku@pm.me>
2023-08-16 00:16:40 +0300

    chore: Project -> Experience

--- components/Sections/Experiences.vue
+++ components/Sections/Experiences.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 defineProps({
-  projects: { type: Array, required: true },
+  experiences: { type: Array, required: true },
 });
 </script>
 <template>
   <div v-motion-slide-visible-once-bottom class="py-4">
-    <h2 class="text-2xl text-neutral-500">Projects I am working on</h2>
+    <h2 class="text-2xl text-neutral-500">Experience</h2>
     <div class="mt-4 grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-1">
-      <ProjectCard
-        v-for="project in projects"
-        :key="project.title"
-        :data="project"
+      <ExperienceCard
+        v-for="experience in experiences"
+        :key="experience.title"
+        :data="experience"
       />
     </div>
   </div>
