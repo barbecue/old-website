4e5260163fa8f7bb5f3aa086cfdb91d19924ff49
BBQ <barbeku@pm.me>
2023-08-16 00:17:42 +0300

    fix: Color fix on error page

--- error.vue
+++ error.vue
@@ -3,5 +3,7 @@ const handleError = () => clearError({ redirect: "/" });
 </script>
 
 <template>
-  <button class="p-2" @click="handleError">are you lost?</button>
+  <button class="p-2 text-black dark:text-white" @click="handleError">
+    are you lost?
+  </button>
 </template>
